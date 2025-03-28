b77b1d3d6c94a857ea4738d0857a5c4b9af80cff
Daniel <daniel_cardona82231@elpoli.edu.co>
2025-03-28 15:40:34 -0500

    funcionalidad carrito

--- src/main/java/com/example/taller2sidatech/Controller/HomeController.java
+++ src/main/java/com/example/taller2sidatech/Controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.example.taller2sidatech.Controller;
 
+import com.example.taller2sidatech.Model.Entity.Compra;
+import com.example.taller2sidatech.Model.Entity.DetalleCompra;
+import com.example.taller2sidatech.Model.Entity.Producto;
 import com.example.taller2sidatech.Service.ProductoService;
 import jdk.jfr.Registered;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -15,6 +20,9 @@ public class HomeController {
 
     @Autowired
     private ProductoService productoService;
+
+    List<DetalleCompra> detalles = new ArrayList<>();
+    Compra compra = new Compra();
 
     @GetMapping("")
     public String home(Model model) {
@@ -24,7 +32,37 @@ public class HomeController {
     }
 
     @GetMapping("/productohome/{id}")
-    public String productoHome(@PathVariable Integer id){
+    public String productoHome(@PathVariable Integer id, Model model){
+
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional = productoService.get(id);
+        producto = productoOptional.get();
+
+        model.addAttribute("producto", producto);
         return "usuario/productohome";
+    }
+
+    @PostMapping("/cart")
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
+        DetalleCompra detalleCompra = new DetalleCompra();
+        Producto producto = new Producto();
+        double sumaTotal=0;
+
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+
+        detalleCompra.setCantidad(cantidad);
+        detalleCompra.setPrecio(producto.getPrecio());
+        detalleCompra.setNombre(producto.getNombre());
+        detalleCompra.setTotal(producto.getPrecio() * cantidad);
+        detalleCompra.setProducto(producto);
+
+        detalles.add(detalleCompra);
+        sumaTotal= detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+        compra.setTotal(sumaTotal);
+        model.addAttribute("cart", detalles);
+        model.addAttribute("compra", compra);
+
+        return "usuario/carrito";
     }
 }
