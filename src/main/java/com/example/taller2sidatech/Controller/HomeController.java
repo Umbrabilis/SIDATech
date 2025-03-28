25290fd9d4273e0daf221a8010d0e1e4d65695c8
Daniel <daniel_cardona82231@elpoli.edu.co>
2025-03-28 14:39:38 -0500

    vista de cada producto

---
+++ src/main/java/com/example/taller2sidatech/Controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.taller2sidatech.Controller;
+
+import com.example.taller2sidatech.Service.ProductoService;
+import jdk.jfr.Registered;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("")
+    public String home(Model model) {
+
+        model.addAttribute("productos", productoService.findAll());
+        return "usuario/home";
+    }
+
+    @GetMapping("/productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+        return "usuario/productohome";
+    }
+}
