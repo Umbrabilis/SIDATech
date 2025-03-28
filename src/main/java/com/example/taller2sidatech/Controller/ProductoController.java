4fc9966997e235f032d52c2c109352ed2e172fed
Daniel <daniel_cardona82231@elpoli.edu.co>
2025-03-28 13:10:18 -0500

    Mostrar imagenes en administrador

--- src/main/java/com/example/taller2sidatech/Controller/ProductoController.java
+++ src/main/java/com/example/taller2sidatech/Controller/ProductoController.java
@@ -66,14 +66,14 @@ public class ProductoController {
 
     @PostMapping("/update")
     public String update(Producto producto,  @RequestParam("img") MultipartFile file) throws IOException {
+        Producto p = new Producto();
+        p = productoService.get(producto.getId()).get();
+
         if (file.isEmpty()){ //editamos producto sin cambiar imagen
-            Producto p = new Producto();
-            p = productoService.get(producto.getId()).get();
+
             producto.setImagen(p.getImagen());
         }
         else { //editamos producto cambiando imagen
-            Producto p = new Producto();
-            p = productoService.get(producto.getId()).get();
 
             if (!p.getImagen().equals("default.jpg")){
                 upload.deleteImage(p.getImagen());
@@ -82,6 +82,8 @@ public class ProductoController {
             String nombreImagen = upload.saveImage(file);
             producto.setImagen(nombreImagen);
         }
+
+        producto.setUsuario(p.getUsuario());
         productoService.update(producto);
 
         return "redirect:/productos";
